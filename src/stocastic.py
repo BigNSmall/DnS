0b21ca1d8db241101c1583bf704a70490f251b54
Ahnyeongjun <toinin@naver.com>
2024-08-26 11:30:39 +0900

    FIX stocastic return type

--- src/stocastic.py
+++ src/stocastic.py
@@ -25,11 +25,13 @@ def stochastic_slow(
     fastk = 100 * ((data["종가"] - low_min) / (high_max - low_min))
     slowk = fastk.rolling(window=slowk_period, min_periods=1).mean()
     slowd = slowk.rolling(window=slowd_period, min_periods=1).mean()
-    slowk = slowk[:window_size]
+    #slowk = slowk[:window_size]
     return {
-        "slowd": np.array(slowd.fillna(0.00001).to_numpy()),
-        "slowk": np.array(slowk.fillna(0.00001).to_numpy()),
+        "slowd": slowd,
+        "slowk": slowk,
     }
+    #return pd.DataFrame({"slowd": slowd, "slowk": slowk}, index=data.index)
+
 
 
 def stochastic_fast(
@@ -50,9 +52,10 @@ def stochastic_fast(
     fastd = fastk.rolling(window=fastd_period, min_periods=1).mean()
 
     fastd = fastd[:window_size]
-    fastk = fastk[:window_size]
-    # return {"fastk": fastk, "fastd": fastd}
-    return pd.DataFrame({"fastk": fastk, "fastd": fastd}, index=data.index)
+    #fastd = fastd[:window_size]
+    #fastk = fastk[:window_size]
+    return {"fastk": fastk, "fastd": fastd}
+    #return pd.DataFrame({"fastk": fastk, "fastd": fastd}, index=data.index)
 
 
 # 사용 예시
